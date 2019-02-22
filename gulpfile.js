e1dad918f97ad7ef890d31f04bfa648df997e0fd
Irhad Kulanic <irhad.kulanic@adesso.de>
2019-02-22 09:49:54 +0100

    gulp anpassung

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('default', () =>
-    gulp.src('src/main.css')
+    gulp.src('src/*.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions', 'ie 6-8', 'Firefox > 20'],
             grid: true,
             cascade: false
         }))
         .pipe(gulp.dest('./dist')),
-        gulp.src('src/flex.html')
+        gulp.src('src/*.html')
         .pipe(gulp.dest('./dist'))
 );
